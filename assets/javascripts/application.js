00f8a52d8fba60bb8997230d03588e5ac1bbd897
Shawn Oakley <shawnoakley@gmail.com>
2014-01-06 13:49:20 -0500

    Adds disqualify link, functionality.

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -30,36 +30,44 @@ AngryCats = Backbone.Collection.extend({
       cat.set('rank', rank);
       ++rank;
     });
-
+    
     this.on('add', function(cat){
-    	if ( ! cat.get('rank')) {
-    		var error = Error("Cat must have a rank defined before being added to the collection");
-    		error.name = "NoRankError";
-    		throw error;
-    	}
+      if( ! cat.get('rank') ){
+        var error = Error("Cat must have a rank defined before being added to the collection");
+        error.name = "NoRankError";
+        throw error;
+      }
     });
-
+    
     var self = this;
 
-  	MyApp.on('rank:up', function(cat){
-    	if (cat.get('rank') == 1) {
-    		// can't increase rank of top-ranked cat
-    		return true;
-    	}
-    	self.rankUp(cat);
-    	self.sort();
-    	self.trigger('reset');
-  	});
- 
-  MyApp.on('rank:down', function(cat){
-  		if (cat.get('rank') == self.size()) {
-  			// can't decrease rank of lowest ranked cat
-  			return true
-  		}
-  		self.rankDown(cat);
-  		self.sort();
-  		self.trigger('reset');
-  	});
+    MyApp.vent.on("rank:up", function(cat){
+      if (cat.get('rank') == 1) {
+        // can't increase rank of top-ranked cat
+        return true;
+      }
+      self.rankUp(cat);
+      self.sort();
+    });
+
+    MyApp.vent.on("rank:down", function(cat){
+      if (cat.get('rank') == self.size()) {
+        // can't decrease rank of lowest ranked cat
+        return true;
+      }
+      self.rankDown(cat);
+      self.sort();
+    });
+    
+    MyApp.vent.on("cat:disqualify", function(cat){
+      var disqualifiedRank = cat.get('rank');
+      var catsToUprank = self.filter(
+        function(cat){ return cat.get('rank') > disqualifiedRank; }
+      );
+      catsToUprank.forEach(function(cat){
+        cat.rankUp();
+      });
+    });
   },
 
   comparator: function(cat){
@@ -97,17 +105,27 @@ AngryCatView = Backbone.Marionette.ItemView.extend({
   
   events: {
     'click .rank_up img': 'rankUp',
-    'click .rank_down img': 'rankDown'
+    'click .rank_down img': 'rankDown',
+    'click a.disqualify': 'disqualify'
+  },
+
+  initialize: function() {
+  	this.listenTo(this.model, "change:votes", this.render);
   },
   
 	rankUp: function(){
 		this.model.addVote();
-	  MyApp.trigger('rank:up', this.model);
+		MyApp.trigger('rank:up', this.model);
 	},
 	 
 	rankDown: function(){
 		this.model.addVote();
 	  MyApp.trigger('rank:down', this.model);
+	},
+
+	disqualify: function() {
+		MyApp.vent.trigger('cat:disqualify', this.model);
+		this.model.destroy();
 	}
 });
 
